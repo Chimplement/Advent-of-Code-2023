83b53f5bd8afd1831932e09a1c2c9869664b8f2b
Chimplement <tyranosaurusrex0@gmail.com>
2023-12-11 21:06:41 +0100

    shorter day 11 golf

--- golf/11.py
+++ golf/11.py
@@ -1,4 +1,4 @@
 u=open(0).read().split("\n")
 e=lambda x,y:sum(999999*(n<x)for n in y)
 P=sum([[(e(x,[i for i in range(len(u[0]))if all(0 for l in u if l[i]=="#")])+x,e(y,[i for i,l in enumerate(u)if not"#"in l])+y)for x,h in enumerate(l)if h=="#"]for y,l in enumerate(u)],[])
-print(sum(sum([[abs(q[0]-p[0])+abs(q[1]-p[1])for q in P]for p in P],[]))//2)
+print(sum(sum(abs(q[0]-p[0])+abs(q[1]-p[1])for q in P)for p in P)//2)
